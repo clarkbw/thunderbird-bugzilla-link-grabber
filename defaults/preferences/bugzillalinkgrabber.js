30c09e89da03911b38fa0bd56ea57a0e836c0c78
Bryan Clark <clarkbw@gnome.org>
2008-04-04 02:26:39 -0400

    initial commit

---
+++ defaults/preferences/bugzillalinkgrabber.js
@@ -0,0 +1,9 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.bugzillalinkgrabber@bryan.clark.description", "chrome://bugzillalinkgrabber/locale/bugzillalinkgrabber.properties");
+pref("extensions.bugzillalinkgrabber@bryan.clark.boolpref", false);
+pref("extensions.bugzillalinkgrabber@bryan.clark.intpref", 0);
+pref("extensions.bugzillalinkgrabber@bryan.clark.stringpref", "A string");
+pref("extensions.bugzillalinkgrabber@bryan.clark.default.name", "bugzilla.mozilla.org");
+pref("extensions.bugzillalinkgrabber@bryan.clark.default.url", "https://bugzilla.mozilla.org/show_bug.cgi?id=%s");
+pref("extensions.bugzillalinkgrabber@bryan.clark.bugzillas.bugzilla.mozilla.org", "https://bugzilla.mozilla.org/show_bug.cgi?id=%s");
+pref("extensions.bugzillalinkgrabber@bryan.clark.bugzillas.bugzilla.gnome.org", "http://bugzilla.gnome.org/show_bug.cgi?id=%s");
